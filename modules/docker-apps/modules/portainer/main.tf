5c911b5b40bb1f8c6354f2206f3ab6922af578a2
divStar <divStar@gmail.com>
2025-11-23 03:28:26 +0100

    feat(portainer): added always-auto-oauth using mastercard/restapi and terraform-zitadel-provider

--- modules/docker-apps/modules/portainer/main.tf
+++ modules/docker-apps/modules/portainer/main.tf
@@ -13,15 +13,61 @@ terraform {
       source  = "zitadel/zitadel"
       version = ">= 2.3.0"
     }
+    restapi = {
+      source  = "mastercard/restapi"
+      version = ">= 2.0.1"
+    }
   }
+}
+
+locals {
+  base_domain = "my.world"
+  portainer_jwt = restapi_object.portainer_jwt.api_data["jwt"]
 }
 
 # `zitadel` provider set up.
 provider "zitadel" {
-  domain           = "zitadel.my.world"
+  domain           = "zitadel.${local.base_domain}"
   insecure         = "false"
   port             = "443"
   jwt_profile_file = "${path.module}/../../admin_key.json"
+}
+
+# `restapi` provider set up.
+provider "restapi" {
+  alias = "unauthorized_restapi"
+  uri                  = "https://portainer.${local.base_domain}"
+  write_returns_object = true
+  debug                = true
+
+  headers = {
+    "Content-Type" = "application/json"
+  }
+}
+
+# `restapi` provider set up.
+provider "restapi" {
+  alias = "jwt_restapi"
+  uri                  = "https://portainer.${local.base_domain}"
+  write_returns_object = true
+  debug                = true
+
+  headers = {
+    "Content-Type" = "application/json"
+     "Authorization" = "Bearer ${local.portainer_jwt}"
+  }
+
+  create_method  = "PUT"
+  update_method  = "PUT"
+  destroy_method = "PUT"
+}
+
+resource "restapi_object" "portainer_jwt" {
+  provider = restapi.unauthorized_restapi
+  create_method = "POST"
+  path = "/api/auth"
+  data = file("${path.module}/portainer-get-jwt.secret.json")
+  object_id = "portainer_token"
 }
 
 # Call to the [OIDC module](../../../common/modules/oidc/README.md) to create the necessary resources in Zitadel.
@@ -34,12 +80,25 @@ module "portainer_web_ui_oidc" {
   app_type         = "OIDC_APP_TYPE_WEB"
   auth_method_type = "OIDC_AUTH_METHOD_TYPE_BASIC"
 
-  redirect_uris             = ["https://portainer.my.world/"]
+  redirect_uris             = ["https://portainer.${local.base_domain}/"]
   response_types            = ["OIDC_RESPONSE_TYPE_CODE"]
   grant_types               = ["OIDC_GRANT_TYPE_AUTHORIZATION_CODE"]
-  post_logout_redirect_uris = ["https://portainer.my.world"]
+  post_logout_redirect_uris = ["https://portainer.${local.base_domain}/"]
 
-  admin_user = "igor.voronin@my.world"
+  admin_user = "igor.voronin@${local.base_domain}"
+}
+
+resource "restapi_object" "portainer_settings" {
+  depends_on = [module.portainer_web_ui_oidc]
+
+  provider = restapi.jwt_restapi
+  path = "/api/settings"
+  data = templatefile("${path.module}/portainer-set-oauth-settings.json.tftpl", {
+    client_id = module.portainer_web_ui_oidc.client_id
+    client_secret = module.portainer_web_ui_oidc.client_secret
+    base_domain = local.base_domain
+  })
+  object_id = "portainer_settings"
 }
 
 # `client_id` for further use.
