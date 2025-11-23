fbd1906b76dcb98d19963d81fb47fc249ae21420
divStar <divStar@gmail.com>
2025-11-24 00:39:08 +0100

    Removed unnecessary project_id output from common/oidc

--- modules/common/modules/oidc/outputs.tf
+++ modules/common/modules/oidc/outputs.tf
@@ -9,8 +9,3 @@ output "client_secret" {
   value       = zitadel_application_oidc.this.client_secret
   sensitive   = true
 }
-
-output "project_id" {
-  description = "ID of the newly created project in Zitadel"
-  value       = zitadel_project.this.id
-}
